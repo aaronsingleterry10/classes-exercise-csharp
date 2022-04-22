da9e524b0b82e137b8e3ea32d0d8c5a55ce4adf3
Aaron Singleterry <aaronsingleterry10@gmail.com>
2022-04-21 23:09:39 -0500

    moved stopwatch to its own file

--- Program.cs
+++ Program.cs
@@ -7,51 +7,6 @@ using System.Diagnostics;
 
 namespace ClassesExercise
 {
-    public class Stopwatch
-    {
-        private Nullable<DateTime> _startTime;
-        private DateTime _stopTime;
-        private TimeSpan _duration;
-
-        public void StartTime()
-        {
-            if (_stopTime > DateTime.MinValue)
-            {
-                _startTime = null;
-                _startTime = DateTime.Now;
-            }
-            else if (_startTime == DateTime.MinValue)
-            {
-                _startTime = DateTime.Now;
-            }
-            else
-            {
-                throw new InvalidOperationException("stopwatch started already");
-            }
-            
-        }
-
-        public void StopTime()
-        {
-            //if (_stopTime > DateTime.MinValue)
-            //{
-            //    _stopTime = DateTime.Now;
-            //} 
-            //else
-            //{
-                _stopTime = DateTime.Now;
-            //}
-        }
-
-        public TimeSpan Duration
-        {
-            get
-            {
-                _duration = _stopTime.Subtract((DateTime)_startTime);
-                return _duration;
-            }
-        }
-    }
     internal class Program
     {
         static void Main(string[] args)
---
+++ Stopwatch.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace ClassesExercise
+{
+    public class Stopwatch
+    {
+        private Nullable<DateTime> _startTime;
+        private DateTime _stopTime;
+        private TimeSpan _duration;
+
+        public void StartTime()
+        {
+            if (_stopTime > DateTime.MinValue)
+            {
+                _startTime = null;
+                _startTime = DateTime.Now;
+            }
+            else if (_startTime == DateTime.MinValue)
+            {
+                _startTime = DateTime.Now;
+            }
+            else
+            {
+                throw new InvalidOperationException("stopwatch started already");
+            }
+            
+        }
+
+        public void StopTime()
+        {
+            //if (_stopTime > DateTime.MinValue)
+            //{
+            //    _stopTime = DateTime.Now;
+            //} 
+            //else
+            //{
+                _stopTime = DateTime.Now;
+            //}
+        }
+
+        public TimeSpan Duration
+        {
+            get
+            {
+                _duration = _stopTime.Subtract((DateTime)_startTime);
+                return _duration;
+            }
+        }
+    }
+}
