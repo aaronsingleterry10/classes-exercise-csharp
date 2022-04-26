4df4a0aa2bca61c72d9642e36e17e79298bd3409
Aaron Singleterry <aaronsingleterry10@gmail.com>
2022-04-25 22:12:27 -0500

    added console logic

--- Stopwatch.cs
+++ Stopwatch.cs
@@ -7,17 +7,19 @@ namespace ClassesExercise
         private DateTime _startTime;
         private DateTime _stopTime;
         private TimeSpan _duration;
+        private bool _hasStarted = false;
 
         public void StartTime()
         {
-            if (_startTime == DateTime.MinValue)
+            if (!_hasStarted)
             {
                 _startTime = DateTime.Now;
+                _hasStarted = true;
             }
-            else if (_stopTime > DateTime.MinValue)
-            {
-                _startTime = DateTime.Now;
-            }
+            //else if (_stopTime > DateTime.MinValue)
+            //{
+            //    _startTime = DateTime.Now;
+            //}
             else
             {
                 throw new InvalidOperationException("stopwatch started already");
@@ -27,14 +29,8 @@ namespace ClassesExercise
 
         public void StopTime()
         {
-            //if (_stopTime > DateTime.MinValue)
-            //{
-            //    _stopTime = DateTime.Now;
-            //} 
-            //else
-            //{
-                _stopTime = DateTime.Now;
-            //}
+            _stopTime = DateTime.Now;
+            _hasStarted = false;
         }
 
         public TimeSpan Duration
